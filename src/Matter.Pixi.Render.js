7926375c1cbb9b05c99892a5060636b524a808bc
Jaime López López <jaloplo@gmail.com>
2018-11-20 23:53:06 +0100

    + Added functionality for rendering Matter.js bodies

--- src/Matter.Pixi.Render.js
+++ src/Matter.Pixi.Render.js
@@ -14,7 +14,21 @@ var Render = {};
     
     // Matter.js shortcuts
     var Events = Matter.Events,
-        Common = Matter.Common;
+        Common = Matter.Common,
+        Composite = Matter.Composite;
+
+
+    let _requestAnimationFrame,
+        _cancelAnimationFrame;
+
+    if (typeof window !== 'undefined') {
+        _requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame
+                                      || window.mozRequestAnimationFrame || window.msRequestAnimationFrame 
+                                      || function(callback){ window.setTimeout(function() { callback(Common.now()); }, 1000 / 60); };
+   
+        _cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame 
+                                      || window.webkitCancelAnimationFrame || window.msCancelAnimationFrame;
+    }
 
 
     Render.add = function(render, resource) {
@@ -23,13 +37,18 @@ var Render = {};
         });
     };
 
+    
+    Render.addChildren = function(render, sprite) {
+        render.app.stage.addChild(sprite);
+    };
+
+
     Render.create = function(options) {
         var defaults = {
-            options: {
-                app: null,
-                canvas: null,
-                target: null,
-            }
+            app: null,
+            canvas: null,
+            target: null,
+            sprites: {},
         };
 
         var render = Common.extend(defaults, options);
@@ -40,5 +59,64 @@ var Render = {};
 
         return render;
     };
+
+
+    Render.sprite = function(render, body) {
+        var resource = body.render.sprite.resource;
+        var texture = body.render.sprite.texture;
+
+        var pixi = {};
+        if(resource) {
+            if(texture) {
+                pixi = new PIXI.Sprite(Loader.resources[resource].textures[texture]);
+            } else {
+                pixi = new PIXI.Sprite(Loader.resources[resource].texture);
+            }
+            
+            pixi.anchor.set(0.5);
+            pixi.x = body.position.x;
+            pixi.y = body.position.y;
+            pixi.rotation = body.angle;
+            body.width = pixi.width;
+            body.height = pixi.height;
+
+            render.sprites[body.id] = pixi;
+
+            render.app.stage.addChild(pixi);
+        }
+
+        return pixi;
+    };
+
+
+    Render.run = function(render, engine) {
+        var bodies = Composite.allBodies(engine.world);
+        for (var i = 0; i < bodies.length; i++) {
+            var body = bodies[i];
+            if(body.render && body.render.sprite) {
+                Render.sprite(render, body);
+            }
+        }
+
+        Events.on(engine, 'beforeUpdate', function() {
+            var bodies = Composite.allBodies(engine.world);
+            for(var i = 0; i < bodies.length; i++) {
+                var body = bodies[i];
+                var pixi = null || render.sprites[body.id];
+                if(pixi) {
+                    pixi.x = body.position.x;
+                    pixi.y = body.position.y;
+                    pixi.rotation = body.angle;
+                }
+            }
+        });
+        
+        (function loop(time){
+            render.frameRequestId = _requestAnimationFrame(loop);
+            render.app.renderer.render(render.app.stage);
+        })();
+
+        return render;
+    }
 
 })();
