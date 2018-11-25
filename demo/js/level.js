ad0390b0951ad3d0c2e563b7fff9e72c630c6818
Jaime López López <jaloplo@gmail.com>
2018-11-25 21:19:03 +0100

    + Added level script

---
+++ demo/js/level.js
@@ -0,0 +1,13 @@
+var Level = {};
+
+(function() {
+    
+    Level.create = function(data, items) {
+        var _level = {
+            data: data,
+            items: items,
+        };
+
+        return _level;
+    };
+})();
